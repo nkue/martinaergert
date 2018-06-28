d8929db2d897956129f6dce8011406564cf9c8a1
nilkue <nils.kuechler@sinnerschrader.com>
2018-06-28 11:54:40 +0200

    add first implementation of search bar feature

---
+++ search.js
@@ -0,0 +1,114 @@
+// function onSearchClick() {
+//     let search = prompt('What are you looking for?')
+
+//     console.log(search);
+// }
+
+// function onSearchClick() {
+//     const searchOverlay = '<div class="search-overlay"><input type="search" class="search" /><input  class="button button--submit" type="submit" value="Search" /></div>'
+    
+//     document.getElementById("search-form").innerHTML = searchOverlay;
+// }
+
+// (function displaySearchButton() {
+//     const searchButton = [
+//         '<div class="search-button" id="search-button">',
+//             '<input class="button button--submit" type="submit" value="Search" onclick="onSearchClick()" />',
+//         '</div>'
+//     ].join("\n");
+
+//     document.getElementById("search-form").innerHTML = searchButton;
+
+//     function onSearchClick() {
+//         const searchOverlay = [
+//             '<div class="search-overlay">',
+//                 '<input type="search" class="text-input text-input--search" list="eastereggs" id="search-input" />',
+//                 '<datalist class="form-options form-options--search" id="eastereggs">',
+//                     '<option class="form-option form-option--search" value="duck" />',
+//                     '<option class="form-option form-option--search" value="1&#8364; in die Kasse" />',
+//                     '<option class="form-option form-option--search" value="Aberdeen bei Sonne" />',
+//                     '<option class="form-option form-option--search" value="Pato" />',
+//                     '<option class="form-option form-option--search" value="SPAM" />',
+//                 '</datalist>',
+//                 '<input class="button button--submit" type="submit" value="Search" onclick="executeSearch()" id="search"/>',
+//             '</div>'
+//         ].join("\n");
+    
+//         document.getElementById("search-button").innerHTML = searchOverlay;
+//     };
+    
+//     function executeSearch() {
+//         const searchValue = document.getElementById("search-input").value;
+//         alert(searchValue.toString());
+//     };
+// }());
+
+(function onSearchClick() {
+
+    class ReplaceAndShow {
+        constructor(parent, child, main) {
+            this.parent = parent,
+            this.child = child,
+            this.main = main
+        }
+
+        change() {
+            const parentElement = document.querySelector(this.parent);
+            const oldChild = parentElement.querySelector(this.child);
+            const newChild = document.createElement("input");
+            newChild.type = "button";
+            newChild.classList.add("button3");
+            newChild.addEventListener("click", this.show);
+
+            parentElement.replaceChild(newChild, oldChild);
+        }
+
+        create() {
+            const mainElement = document.querySelector(this.main);
+            const searchForm = document.querySelector(this.parent);
+            const searchBar = searchForm.cloneNode(true);
+            searchBar.classList.add("searchbar");
+
+            mainElement.appendChild(searchBar);
+        }
+
+        show() {
+            const searchBar = document.querySelector(".searchbar");
+            searchBar.classList.add("visible");
+        }
+    }
+
+    new ReplaceAndShow(".form--search", ".button", ".first-level-section--main-content").change();
+    new ReplaceAndShow(".form--search", ".button", ".first-level-section--main-content").create();
+
+    // function replaceSearchButton(formclass) {
+    //     const searchForm = document.querySelector(formclass);
+    //     const oldSearchButton = searchForm.querySelector(".button");
+    //     const parentElement = oldSearchButton.parentNode;
+    //     const searchButton = document.createElement("input");
+    //     searchButton.type = "button";
+    //     console.log("searchButton", searchButton, "parent", parentElement);
+
+    //     searchButton.classList.add("button1");
+    //     searchButton.addEventListener("click", openSearchBar);
+        
+    //     parentElement.replaceChild(searchButton, oldSearchButton);
+    // }
+
+    // function createSearchBar(mainclass) {
+    //     console.log("this happened");
+    //     const main = document.querySelector(mainclass);
+    //     const searchForm = document.querySelector(".form--search");
+    //     const searchBar = searchForm.cloneNode(true);
+    //     console.log("searchbar", searchBar);
+    //     searchBar.classList.add("searchbar");
+
+    //     main.appendChild(searchBar);
+    // }
+
+    // function openSearchBar() {
+    //     const searchBar = document.querySelector(".searchbar");
+    //     searchBar.classList.add("visible");
+    // }
+}());
+

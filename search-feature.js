d7eeca398a7bc0ae3e95fd6618ab276305447d4b
nilkue <nils.kuechler@sinnerschrader.com>
2018-07-20 09:30:05 +0200

    ensure local scope, minor refactoring

--- search-feature.js
+++ search-feature.js
@@ -1,100 +1,109 @@
-var searchFeature = (function () {
-    const element = constructElement(
-        "form",
-        {
-            name: "search",
-            role: "search",
-            method: "POST",
-            class: "searchbar"
-        },
-        [
-            child1 = constructElement(
-                "input",
-                {
-                    class: "text-input text-input--search",
-                    id: "search",
-                    type: "search",
-                    name: "q",
-                    placeholder: "looky, looky - findy, findy!",
-                    list: "eastereggs",
-                    size: "50"
-                }
-            ),
-            child2 = constructElement(
-                "datalist",
-                {
-                    class: "form-options form-options--search",
-                    id: "eastereggs"
-                },
-                [
-                    option1 = constructElement(
-                        "option",
-                        {
-                            class: "form-option form-option--search",
-                            value: "duck"
-                        }
-                    )
-                ]
-            ),
-            child3 = document.createElement("div")
-        ]
-    );
+(function () {
+    "use strict"
 
-    function onSearchClick() {
-        element.classList.add("visible");
-    }
-
-    function constructElement(tagName, attributes = {}, children = []) {
-        const newElement = document.createElement(tagName);
-        Object.entries(attributes).forEach(attr => {
-            newElement.setAttribute(attr[0], attr[1]);
-        });
-        Object.keys(children).forEach(child => {
-            newElement.appendChild(children[child]);
-        });
+    window.searchFeature = function searchFeature() {
         
-        return newElement;
-    };
-
-    function appendElement() {
-        if (element) {
-            const mainElement = document.querySelector(".search-feature");
-            mainElement.appendChild(element);
-        } else return;
-    };
-
-    function replaceOldSearch() {
-        const parent = document.querySelector(".replace-search");
-        const newChild = constructElement(
-            "input",
+        const element = constructElement(
+            "form",
             {
                 name: "search",
-                role: "search-button",
-                type: "submit",
+                role: "search",
                 method: "POST",
-                value: "Search",
-                class: "search-button"
+                class: "searchbar"
             },
-            []
+            [
+                constructElement(
+                    "input",
+                    {
+                        class: "text-input text-input--search",
+                        id: "search",
+                        type: "search",
+                        name: "q",
+                        placeholder: "looky, looky - findy, findy!",
+                        list: "eastereggs",
+                        size: "50"
+                    }
+                ),
+                constructElement(
+                    "datalist",
+                    {
+                        class: "form-options form-options--search",
+                        id: "eastereggs"
+                    },
+                    [
+                        constructElement(
+                            "option",
+                            {
+                                class: "form-option form-option--search",
+                                value: "duck"
+                            }
+                        )
+                    ]
+                ),
+                constructElement(
+                    "input",
+                    {
+                        class: "button button--submit",
+                        type: "submit",
+                        id: "submit",
+                        value: "",
+                        accesskey: "enter"
+                    }
+                )
+            ]
         );
-
-        if (parent) {
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            }
-
-            parent.appendChild(newChild);
-        } else {
-            const mainElement = document.querySelector(".add-search-button");
-            mainElement.appendChild(newChild);
-        }
-
-        newChild.addEventListener("click", onSearchClick);
-    }
-
-    appendElement();
-    replaceOldSearch();
     
-})
-
-searchFeature();
+        function showElement(element) {
+            element.classList.add("visible");
+        }
+    
+        function constructElement( tagName, attributes = {}, children = [] ) {
+            const newElement = document.createElement( tagName );
+            Object.entries( attributes ).forEach(([ prop, value ]) => {
+                newElement.setAttribute( prop, value );
+            });
+            children.forEach(child => newElement.appendChild( child ));
+            
+            return newElement;
+        }
+    
+        function appendElement(element) {
+            const mainElement = document.querySelector(".search-feature");
+            if (mainElement) {
+                mainElement.appendChild(element);
+            }
+        }
+    
+        function replaceOldSearch() {
+            const parent = document.querySelector(".replace-search");
+            const newChild = constructElement(
+                "input",
+                {
+                    name: "search",
+                    role: "search-button",
+                    type: "submit",
+                    method: "POST",
+                    value: "Search",
+                    class: "search-button"
+                }
+            );
+    
+            if (parent) {
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
+    
+                parent.appendChild(newChild);
+            } else {
+                const mainElement = document.querySelector(".add-search-button");
+                mainElement.appendChild(newChild);
+            }
+    
+            newChild.addEventListener("click", () => showElement(element));
+        }
+    
+        appendElement(element);
+        replaceOldSearch();
+        
+    };
+})();

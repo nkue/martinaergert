b71f401ecd1506d2e5a260eb30d679e3bcf2f7d9
nilkue <nils.kuechler@sinnerschrader.com>
2018-07-23 18:09:04 +0200

    improve reusablity

--- feature-search-refactoring.js
+++ feature-search-refactoring.js
@@ -1,12 +1,38 @@
-(() => {
+(function searchFeature() {
 
-    const config = {
-        tagName: "input",
-        childType: "button",
-        childClass: "button3",
-        eventType: "click",
-        eventClass: "searchbar"
-    }
+    function createElement(tagName, attributes = {}, children = []) {
+        const newElement = document.createElement(tagName);
+        const mainElement = document.querySelector(".first-level-section--main-content");
+        const childElements = children.map(child1 => {
+            console.log(children);
+            document.createElement(child1[0]);
+        });
+        console.log(children);
+        console.log(childElements);
+        newElement.type = attributes.childType;
+        newElement.classList.add(attributes.childClass);
+        mainElement.appendChild(newElement);
+        mainElement.appendChild(childElements);
+
+        return newElement;
+    };
+
+    createElement(
+        tagName = "div",
+        attributes = {
+            className: "test",
+            childClass: "testChild",
+            childType: "span"
+        },
+        children = [
+            something = [
+                tagName = "div"
+            ],
+            somethingElse = [
+                tagName = "someTest"
+            ] 
+        ]
+    );
 
     class ReplaceAndShowSearchBar {
         constructor({
@@ -30,23 +56,23 @@
             this.config.childClass = config.childClass;
             this.config.eventType = config.eventType;
             this.config.eventClass = config.eventClass;
-            this.toggleElement = this.toggleElement.bind(this);
             this.state = {
-                visible: false,
                 oldParent: "",
                 oldChild: "",
                 newChild: "",
-                clonedElement: ""
+                clonedElement: "",
+                visible: false
             }
         }
 
         doStuff() {
             this.findExistingElement();
             this.constructNewElement();
-            this.addToggleEventListener();
             this.replaceExistingElement();
             this.cloneExistingElement();
             this.appendNewElement();
+            this.addToggleEventListener(this.state.newChild);
+            this.addToggleEventListener(this.state.clonedElement.querySelector(".button"));
         }
 
         findExistingElement() {
@@ -65,16 +91,40 @@
             return this.state.newChild = newElement;
         }
 
-        addToggleEventListener() {
-            this.state.newChild.addEventListener(config.eventType, () => { this.toggleElement() });
+        addToggleEventListener(element) {
+            element.addEventListener(config.eventType, () => this.toggleElement());
+
+            document.addEventListener('keydown', event => {
+                const keyName = event.key;
+              
+                if (this.state.visible === true && keyName === 'Escape') {
+                    event.preventDefault();
+                    this.toggleElement();
+                }
+            });
+
+            document.addEventListener('keypress', (event) => {
+                const keyName = event.key;
+                
+                if (event.ctrlKey && keyName === 'q' && this.state.visible === false) {
+                    event.preventDefault();
+                    this.toggleElement();
+                };
+            });
         }
 
         toggleElement() {
             this.state.clonedElement.classList.toggle("visible");
+            const otherChild = this.state.clonedElement.querySelector(".text-input");
+            otherChild.classList.toggle("hidden");
+            this.state.visible = !this.state.visible;
         }
 
         replaceExistingElement() {
             this.state.oldParent.replaceChild(this.state.newChild, this.state.oldChild);
+            const otherChild = document.querySelector(".text-input");
+
+            otherChild.classList.add("hidden");
         }
 
         cloneExistingElement() {
@@ -91,10 +141,18 @@
         }
     }
 
+    const config = {
+        tagName: "input",
+        childType: "button",
+        childClass: "button",
+        eventType: "click",
+        eventClass: "searchbar"
+    }
+
     new ReplaceAndShowSearchBar({
         parent: ".form--search",
         child: ".button",
         main: ".first-level-section--main-content",
-        config: config
+        config
     }).doStuff();
 })();

1b807e4ed01888ed274b5d5dc6f29ca6181a0772
nilkue <nils.kuechler@sinnerschrader.com>
2018-07-25 18:14:37 +0200

    remove assert tests

--- testing/search-test.js
+++ testing/search-test.js
@@ -10,53 +10,165 @@
             // window.helpers.afterEach(test.name, {
             //     // remove everything deemed useful previously
             // });
-            test( "Both arrays are equal", () => {
-                let value1 = [ "a", "b", "c", "d" ];
-                let value2 = [ "a", "b", "c" ];
-                window.asserts.isEqual( value1, value2 );
+            test( "Both arrays aren't equal", () => {
+                const value1 = [ "a", "b", "c", "d" ];
+                const value2 = [ "a", "b", "c" ];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "Both arrays (2nd is longer) are equal", () => {
-                let value1 = [ "a", "b", "c", "d" ];
-                let value2 = [ "a", "b", "c", "d", "f" ];
-                window.asserts.isEqual( value1, value2 );
+            test( "Both arrays (2nd is longer) aren't equal", () => {
+                const value1 = [ "a", "b", "c", "d" ];
+                const value2 = [ "a", "b", "c", "d", "f" ];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "Both objects are equal", () => {
-                let value1 = { a: { b: 1 }, c: 2 };
-                let value2 = { a: { b: 1 }, c: 2 };
-                window.asserts.isEqual( value1, value2 );
+            test( "Both nested arrays are equal", () => {
+                const value1 = [ "a", "b", "c", [ "d", "e" ], "f" ];
+                const value2 = [ "a", "b", "c", [ "d", "e" ], "f" ];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "Both objects are equal (but different order)", () => {
-                let value1 = { c: 2, a: 2 };
-                let value2 = { a: 1, c: 2 };
-                window.asserts.isEqual( value1, value2 );
+            test( "Both nested arrays aren't equal", () => {
+                const value1 = [ "a", "b", "c", [ "d", "e" ], "f" ];
+                const value2 = [ "a", "b", "c", [ "e", "d" ], "f" ];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "Both values are equal", () => {
-                let value1 = { a: { b: 1 }, c: 2 };
-                let value2 = [ "a", { b: 1}, "c"];
-                window.asserts.isEqual( value1, value2 );
+            test( "Both empty arrays are equal", () => {
+                const value1 = [];
+                const value2 = [];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "NaN is equal to NaN", () => {
-                let value1 = NaN;
-                let value2 = NaN;
-                window.asserts.isEqual( value1, value2 );
+            test( "Both mixed type arrays are equal", () => {
+                const value1 = [ "a", 1, undefined, [ null, true, false ]];
+                const value2 = [ "a", 1, undefined, [ null, true, false ]];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "-0 is equal to 0", () => {
-                let value1 = -0;
-                let value2 = 0;
-                window.asserts.isEqual( value1, value2 );
+            test( "Both mixed type arrays with holes are equal", () => {
+                const value1 = [ "a", 1, undefined, [ null, Array(1), false ]];
+                const value2 = [ "a", 1, undefined, [ null, Array(1), false ]];
+                window.asserts.isEqualArray( value1, value2 );
             });
 
-            test( "0 is equal to 0", () => {
-                let value1 = 0;
-                let value2 = 0;
-                window.asserts.isEqual( value1, value2 );
+            test( "Both mixed type arrays with holes are not equal", () => {
+                const value1 = [ "a", 1, undefined, [ null, Array(1), false ]];
+                const value2 = [ "a", 1, undefined, [ null, Array(2), undefined ]];
+                window.asserts.isEqualArray( value1, value2 );
             });
+
+            test( "Both matrix-shaped arrays are equal", () => {
+                const value1 = [ 
+                    [ -1, 2, 3 ],
+                    [ 4, -5, 6 ],
+                    [ 7, 8, -9 ]
+                ];
+                const value2 = [ 
+                    [ -1, 2, 3 ],
+                    [ 4, -5, 6 ],
+                    [ 7, 8, -9 ]
+                ];
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Both matrix-shaped arrays aren't equal", () => {
+                const value1 = [ 
+                    [ 1, -2, 3 ],
+                    [ 4, 5, 6 ],
+                    [ 7, 8, 9 ]
+                ];
+                const value2 = [ 
+                    [ 1, 2, 3 ],
+                    [ 4, 5, 6 ],
+                    [ 7, 8, 9 ]
+                ];
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Both nested empty arrays are equal", () => {
+                const value1 = [ [], [ [], [] ] ];
+                const value2 = [ [], [ [], [] ] ];
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Both nested empty arrays are not equal", () => {
+                const value1 = [ [], [ [], [] ] ];
+                const value2 = [ [], [ [] ] ];
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Both arrays with holes are equal", () => {
+                const value1 = Array(5);
+                const value2 = Array(5);
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Both arrays with holes aren't equal", () => {
+                const value1 = Array(5);
+                const value2 = Array(7);
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            test( "Is equal doesn't get fooled by an array like object", () => {
+                const value1 = [ "a", "b" ];
+                const value2 = { "0": "a", "1": "b", length: 2 };
+                window.asserts.isEqualArray( value1, value2 );
+            });
+
+            // test( "Both objects are equal", () => {
+            //     const value1 = { a: { b: 1 }, c: 2 };
+            //     const value2 = { a: { b: 1 }, c: 2 };
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "Both mixed and nested objects are not equal", () => {
+            //     const value1 = { a: { b: 1 }, c: 2, f: undefined };
+            //     const value2 = { a: { b: 1 }, c: 2 };
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "Both objects are equal (but different order)", () => {
+            //     const value1 = { c: 2, a: 2 };
+            //     const value2 = { a: 1, c: 2 };
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "Both mixed objects are equal (but different order)", () => {
+            //     const value1 = { c: 2, a: 2, b: undefined, f: false, e: null };
+            //     const value2 = { a: 1, c: 2, e: null, b: undefined, f: false };
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "Both objects (contain arrays and) are equal", () => {
+            //     const value1 = { c: 2, b: [ 1, "a" ], a: 2 };
+            //     const value2 = { a: 1, b: [ 1, "a" ], c: 2 };
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "Both values aren't equal", () => {
+            //     const value1 = { a: { b: 1 }, c: 2 };
+            //     const value2 = [ "a", { b: 1}, "c"];
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "NaN is equal to NaN", () => {
+            //     const value1 = NaN;
+            //     const value2 = NaN;
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "-0 is equal to 0", () => {
+            //     const value1 = -0;
+            //     const value2 = 0;
+            //     window.asserts.isEqual( value1, value2 );
+            // });
+
+            // test( "0 is equal to 0", () => {
+            //     const value1 = 0;
+            //     const value2 = 0;
+            //     window.asserts.isEqual( value1, value2 );
+            // });
 
             // test("A search button was found", () => {
             //     const name = "Search Button";

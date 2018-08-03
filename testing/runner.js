f2f024e20c66c9d9a343b61fcc29398fb23bc2a3
nilkue <nils.kuechler@sinnerschrader.com>
2018-08-03 14:06:37 +0200

    refactor beforeEach, afterEach

--- testing/runner.js
+++ testing/runner.js
@@ -1,41 +1,38 @@
-(function () {
+(function (scope) {
     "use strict";
 
-    window.describe = function describe( name, fn ) {
+    const registry = {
+        beforeEach: "",
+        afterEach: ""
+    };
+
+    scope.describe = function (name, fn) {
         console.log( `${name} started testing` );
         fn();
     };
     
-    window.test = function test( name, fn ) {
-        window.helpers.beforeEach( name );
+    scope.test = function test( name, fn ) {
+        scope.helpers.beforeEach();
         try {
             fn();
             console.log( `      ${name}, awesome!` );
         } catch( error ) {
             console.error( `      ${name} failed with ${error.message}!` );
         }
-        window.helpers.afterEach( name );
+        scope.helpers.afterEach();
     };
 
-    window.helpers = {
-        beforeEach( name ) {
-            const boundBeforeEachConfig = beforeEachConfig.bind(window.teststuff);
-            try {
-                boundBeforeEachConfig();
-                console.log(` basic setup for ${name} `);
-            } catch ( error ) {
-                console.error( ` basic setup failed for ${name} with ${error} `);
-            }
-
+    scope.helpers = {
+        beforeEach() {
+            const bodyContent = document.querySelector("body");
+            const clonedBody = bodyContent.cloneNode(true);
+            registry.beforeEach = clonedBody;
         },
-        afterEach( name ) {
-            const boundAfterEachConfig = afterEachConfig.bind(window.teststuff);
-            try {
-                boundAfterEachConfig();
-                console.log(` tear down for ${name} `);
-            } catch ( error ) {
-                console.error( ` tear down failed after ${name} with ${ error } `);
-            }
+        afterEach() {
+            const htmlTag = document.querySelector("html");
+            const bodyTag = document.querySelector("body");
+            htmlTag.replaceChild(registry.beforeEach, bodyTag);
+            registry.beforeEach = registry.afterEach;
         }
     };
-})();
+})(window);
